56bbef2791fb0821a7260e2537719cdfaf1b867d
Hsieh Ching Hsiang <vita_hsieh@morrisonexpress.com>
2020-12-15 17:26:14 +0800

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html')
+    })
+  ],
+  devServer: {
+    inline: true,
+    contentBase: './src',
+    port: 3000
+  },
+}
